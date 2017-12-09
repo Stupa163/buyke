61b8e71a18594d7705caad4a3a9e4c36d80ebaaa
Stupa163 <remi.bosgaerd@ynov.com>
2017-12-09 22:14:20 +0100

    09/12/2017

--- scripts/script_navbar.js
+++ scripts/script_navbar.js
@@ -2,7 +2,9 @@ $("#loupe").click(function(e){
     if(!$("#search input").is(':visible')){
         e.preventDefault();
         $('#buyke').animate({opacity:'hide'},function(){
-            $('#search input').animate({width:'show'},700);
+            $('#search input').animate({width:'show'},700,function(){
+                $('#search input').focus();
+            });
         })
     }
 })
