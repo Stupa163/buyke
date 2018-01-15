9f6c11abddc3ada633691ee76d694718b141ded0
Stupa163 <remi.bosgaerd@ynov.com>
2018-01-15 20:26:39 +0100

    15/01/2018

--- annonce.php
+++ annonce.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -7,7 +7,7 @@
         <link rel="icon" href="images_systeme/icon.ico">
         <?php
         include 'conn.php';
-        $q_titre=$bdd->prepare("SELECT TITRE from annonces where ID_ANN = ".$_GET['id'].";");
+        $q_titre=$bdd->prepare("SELECT TITRE from annonces where ID_ANN=".$_GET['id'].";");
         $q_titre->execute();
         $titre=$q_titre->fetch();
         echo '<title>'.$titre['TITRE'].'</title>';
@@ -16,17 +16,16 @@
     <body>
         <?php
         include'navbar.php';
+        $q_infos=$bdd->prepare("SELECT * FROM annonces where ID_ANN = ".$_GET['id'].";");
+        $q_infos->execute();
+        $infos=$q_infos->fetch();
         ?>
         <div id="main">
-            <?php
-            $q_infos=$bdd->prepare("SELECT * FROM annonces where ID_ANN = ".$_GET['id'].";");
-            $q_infos->execute();
-            $infos=$q_infos->fetch();
-            echo '<pre>';
-            echo '<div id="photo">
-                    <img src="'.base64_decode($infos['PHOTO']).'">
-                </div>';
-            ?>
+            <div id="photo">
+                <img src="<?php echo base64_decode($infos['PHOTO']); ?>" alt="">
+            </div>
         </div>
+        <script type="text/javascript" src="scripts/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="scripts/script_annonce.js"></script>
     </body>
 </html>
---
+++ scripts/script_annonce.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#photo img').css({'margin-top':(($('#photo').height()-$('#photo img').height())/2)+'px'})
+})
