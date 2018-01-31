7bb74b93f30b185027c7f0506fcc176f6827dd46
Stupa163 <remi.bosgaerd@ynov.com>
2018-01-31 22:17:57 +0100

    31/01/2018

--- scripts/script_ajout_annonce.js
+++ scripts/script_ajout_annonce.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
+    var cours=false;
     $('#ajout').submit(function(e){
         e.preventDefault();
-        $.ajax({
-            url:'add.php',
-            method:'post',
-            data: new FormData(this),
-            cache: false,
-            contentType: false,
-            processData: false
-        }).done(function(data){
-            window.location.replace('annonces.php');
-        })
+        if(!cours){
+            cours=true;
+            $.ajax({
+                url:'add.php',
+                method:'post',
+                data: new FormData(this),
+                cache: false,
+                contentType: false,
+                processData: false
+            }).done(function(data){
+                window.location.replace('annonces.php');
+            })
+        }
     })
 })
