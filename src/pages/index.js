bd8f433aef6be18166157d202a28458cbff3c8cf
a <a>
2020-04-30 16:28:16 +0200

    First homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import ReactPlayer from "react-player"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div className={"home"}>
+    <header>
+      <div className={"logo"}>Buyke</div>
+      <ul>
+        <li>Homme</li>
+        <li>Femme</li>
+        <li>Rouler</li>
+        <li>Recits</li>
+      </ul>
+    </header>
+    <div className={"moto"}>
+      <div className={"main"}>DONNEZ LE TON</div>
+      <div className={"secondary"}>Des essentiels pour rouler aux nouvelles couleurs de la saison.</div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <div className={"video"}>
+      <ReactPlayer
+        url='https://s1.adis.ws/rapha/_vid/soar%20in%20the%20sun%201920x842%2010s/14a7de8b-40a8-4019-adda-9b89a98f74e2/video/0a36bbd1-554f-493d-be42-76ed66ab8619.webm'
+        width={"100%"}
+        height={"100%"}
+        playing
+        loop
+      />
+    </div>
+  </div>
 )
 
 export default IndexPage
