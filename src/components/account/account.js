f8464618b0efcdbd81df68525683d6da180ecbee
a <a>
2020-05-03 11:24:47 +0200

    prout

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,35 +1,54 @@
 import React, { Component } from "react"
+import swal from "sweetalert"
 
 import account from "../../images/account.png"
 
 const Account = () => {
+  async function modalLogout() {
+
+    try {
+      await window.netlifyIdentity.currentUser().logout()
+    } catch (e) {
+      console.log(e)
+    }
+
+    swal({
+      title: "Déconnexion réussie",
+      text: "Vous avez été déconnecté",
+      icon: "success"
+    })
+      .then(() => {
+        window.location.reload()
+      })
+
+  }
 
   function openModal() {
     try {
       window.netlifyIdentity.open()
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
   }
 
   function isLogged() {
     try {
-      return window.netlifyIdentity.currentUser() !== null;
+      return window.netlifyIdentity.currentUser() !== null
     } catch (e) {
-      return false;
+      return false
     }
   }
 
   function renderAccountIcon() {
     if (isLogged()) {
-      return <div>Connecte</div>
+      return <div className={"logout"} onClick={modalLogout}>Deconnexion</div>
     } else {
       return <div><img className={"account"} src={account} alt="" onClick={openModal}/></div>
     }
   }
 
   return (
-    <div>
+    <div className={"account-container"}>
       {renderAccountIcon()}
     </div>
   )
